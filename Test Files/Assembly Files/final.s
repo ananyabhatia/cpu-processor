d8a27fecddc9253d9db71ea09d5698ca994bc82b
Ananya Bhatia <ananya.bhatia@duke.edu>
2024-04-21 19:39:10 -0400

    edit

--- Test Files/Assembly Files/final.s
+++ Test Files/Assembly Files/final.s
@@ -42,48 +42,79 @@ add $r11, $r10, $r0
 
 _start:
 bne $r1, $r0, pressOn
-bne $r2, $r0, pressOff
+#bne $r2, $r0, pressOff
 j _start
 
 pressOn: 
-jal moveDown
+addi $r4, $r0, 9
+loop1:
+addi $r11, $r11, -1
+bne $r11, $r0, loop1
+add $r11, $r10, $r0
+
+addi $r4, $r0, 5
+loop2:
+addi $r11, $r11, -1
+bne $r11, $r0, loop2
+add $r11, $r10, $r0
+
+addi $r4, $r0, 6
+loop3:
+addi $r11, $r11, -1
+bne $r11, $r0, loop3
+add $r11, $r10, $r0
+
+addi $r4, $r0, 10
+loop4:
+addi $r11, $r11, -1
+bne $r11, $r0, loop4
+add $r11, $r10, $r0
+
 addi $r7, $r7, -1
 bne $r7, $r0, pressOn
 add $r7, $r6, $r0
 addi $r3, $r3, 1
 j _start
 
-pressOff:
-mul $r9, $r9, $r3
-jal moveUp
-addi $r9, $r9, -1
-bne $r9, $r0, pressOn
-add $r9, $r8, $r0
-add $r3, $r0, $r0
-j _start
 
-moveDown:
-addi $r4, $r0, 9
-jal delay
-addi $r4, $r0, 5
-jal delay
-addi $r4, $r0, 6
-jal delay
-addi $r4, $r0, 10
-jr $r31
+# jal moveDown
+# addi $r7, $r7, -1
+# bne $r7, $r0, pressOn
+# add $r7, $r6, $r0
+# addi $r3, $r3, 1
+# j _start
 
-moveUp:
-addi $r4, $r0, 10
-jal delay
-addi $r4, $r0, 6
-jal delay
-addi $r4, $r0, 5
-jal delay
-addi $r4, $r0, 9
-jr $r31
+# pressOff:
+# mul $r9, $r9, $r3
+# jal moveUp
+# addi $r9, $r9, -1
+# bne $r9, $r0, pressOn
+# add $r9, $r8, $r0
+# add $r3, $r0, $r0
+# j _start
 
-delay:
-addi $r11, $r11, -1
-bne $r11, $r0, delay
-add $r11, $r10, $r0
-jr $r31
+# moveDown:
+# addi $r4, $r0, 9
+# jal delay
+# addi $r4, $r0, 5
+# jal delay
+# addi $r4, $r0, 6
+# jal delay
+# addi $r4, $r0, 10
+# jr $r31
+
+# moveUp:
+# addi $r4, $r0, 10
+# jal delay
+# addi $r4, $r0, 6
+# jal delay
+# addi $r4, $r0, 5
+# jal delay
+# addi $r4, $r0, 9
+# jr $r31
+
+# delay:
+# addi $r11, $r11, -1
+# bne $r11, $r0, delay
+# add $r11, $r10, $r0
+# jr $r31
