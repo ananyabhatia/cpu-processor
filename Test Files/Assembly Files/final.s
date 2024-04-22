e7b16021b12d206a53f2581d32a7d1f69f34f7fb
Ananya Bhatia <ananya.bhatia@duke.edu>
2024-04-21 23:35:15 -0400

    hit 100 lines

--- Test Files/Assembly Files/final.s
+++ Test Files/Assembly Files/final.s
@@ -34,22 +34,27 @@
 # REGISTER 13 - HOLD # LOOPS TO GO DOWN FOR ONE CUPCAKE MORE (COPY)
 # REGISTER 14 - HOLD # CUPCAKES FROSTED MORE
 # REGISTER 18 - HOLD # CUPCAKES FROSTED TOTAL
+# REGISTER 19 - HOLD DIFF # CYCLES FOR UP
 
 add $r5, $r0, $r0
 add $r2, $r0, $r0
 add $r1, $r0, $r0
 add $r3, $r0, $r0
 add $r14, $r0, $r0
+add $r18, $r0, $r0
+add $r19, $r0, $r0
+add $r4, $r0, $r0
 addi $r29, $r0, 4095 
 addi $r6, $r0, 14
 addi $r7, $r0, 14
 addi $r8, $r0, 15
 addi $r9, $r0, 15
-addi $r12, $r0, 20
-addi $r13, $r0, 20
+addi $r12, $r0, 18
+addi $r13, $r0, 28
 addi $r10, $r10, 1
 sll $r10, $r10, 17
 add $r11, $r10, $r0
+
 
 _start:
 bne $r1, $r0, pressOn
@@ -83,7 +88,7 @@ bne $r11, $r0, loop12
 add $r11, $r10, $r0
 
 addi $r13, $r13, -1
-bne $r13, $r0, pressOn
+bne $r13, $r0, pressOnMORE
 add $r13, $r12, $r0
 addi $r14, $r14, 1
 j _start
@@ -120,7 +125,10 @@ addi $r3, $r3, 1
 j _start
 
 pressOff: 
-add $r18, $r3, $r4
+add $r18, $r3, $r14
+add $r19, $r0, $r14
+sll $r19, $r19, 2
+add $r9, $r0, $r19
 startLoops:
 addi $r4, $r0, 10
 loop5:
@@ -150,50 +158,26 @@ addi $r9, $r9, -1
 bne $r9, $r0, startLoops
 add $r9, $r8, $r0
 
-addi $r3, $r3, -1
-bne $r3, $r0, startLoops
+addi $r18, $r18, -1
+bne $r18, $r0, startLoops
 j _start
 
+delay:
+addi $r11, $r11, -1
+bne $r11, $r0, delay
+add $r11, $r10, $r0
+jr $r31
 
-
-# jal moveDown
-# addi $r7, $r7, -1
-# bne $r7, $r0, pressOn
-# add $r7, $r6, $r0
-# addi $r3, $r3, 1
-# j _start
-
-# pressOff:
-# mul $r9, $r9, $r3
-# jal moveUp
-# addi $r9, $r9, -1
-# bne $r9, $r0, pressOn
-# add $r9, $r8, $r0
-# add $r3, $r0, $r0
-# j _start
-
-# moveDown:
-# addi $r4, $r0, 9
-# jal delay
-# addi $r4, $r0, 5
-# jal delay
-# addi $r4, $r0, 6
-# jal delay
-# addi $r4, $r0, 10
-# jr $r31
-
-# moveUp:
-# addi $r4, $r0, 10
-# jal delay
-# addi $r4, $r0, 6
-# jal delay
-# addi $r4, $r0, 5
-# jal delay
-# addi $r4, $r0, 9
-# jr $r31
-
-# delay:
-# addi $r11, $r11, -1
-# bne $r11, $r0, delay
-# add $r11, $r10, $r0
-# jr $r31
+backup:
+genCase:
+addi $r5, $r0, 1 #choose direction (WILL TEST THIS LATER)
+addi $r4, $r0, 1 # motor on
+#WAIT
+_waitOn:
+addi $r7, $r7, -1
+bne $r7, $r0, _waitOn
+#END
+add $r7, $r6, $r0
+addi $r4, $r0, 0 # motor off
+addi $r3, $r3, 1 # increment number of cupcakes frosted so far 
+j _start
